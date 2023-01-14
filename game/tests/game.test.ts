967cdccf4a73189b50b0f003479d549f3819ae01
siansrd <sianrobinsondavies@gmail.com>
2023-01-14 15:59:11 +0000

    tests createGuessChecker generator fn

--- game/tests/game.test.ts
+++ game/tests/game.test.ts
@@ -1,5 +1,4 @@
 import { addGuess, createCheckGuess } from "../game_logic"
-const ANSWER = "A"
 const checkGuess = createCheckGuess("A")
 
 describe('checkGuess', () => {
@@ -9,6 +8,18 @@ describe('checkGuess', () => {
 
   test('it should return true if guess and answer are the same', () => {
     expect(checkGuess("B")).toEqual(false)
+  })
+})
+
+describe('createCheckGuess', () => {
+  const ANSWER = "A"
+
+  test('it should return true if the string it is passed matches the string passed to the returned function', () => {
+    expect(createCheckGuess(ANSWER)("A")).toBe(true)
+  })
+
+  test('it should return false if the string it is passed does not match the string passed to the returned function', () => {
+    expect(createCheckGuess(ANSWER)("B")).toBe(false)
   })
 })
 
